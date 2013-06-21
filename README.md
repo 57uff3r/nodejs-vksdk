556726bd2686264f8a909ab9a9276160d9692f03
Andrey Korchak <57uff3r@gmail.com>
2013-06-21 18:55:26 +0700

    переписал SDK на событиях, обновил доку.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ nodejs-vksdk
 Просто скопируйте файл vksdk.js в свой проект и подключите.
 
 ```js
-var vk = require('./vksdk.js');
+var VK = require('./vksdk.js');
 ```
 
 Использование
@@ -17,16 +17,30 @@ var vk = require('./vksdk.js');
 Введите параметры доступа к API: ID приложения и секретный ключ.
 
 ```js
-vk.init('24457747', 'CKBejdjdi93ORZFTgjIZB45Qb');
+var vk = new VK('2306377', 'QYjhHF9YskkskPnKbXhX40RqQ');
 ```
 
-Выполнение запросов осуществляется так: 
+Выполнение запросов осуществляется так:
 ```js
-vk.request('getProfiles', {'uids' : '29894'}, function(response) { 
-        console.log(response.response)
+vk.request('getProfiles', {'uids' : '29894'});
+vk.on('done:getProfiles', function(_o) {
+    console.log(_o);
+});
+
+vk.request('places.getCountryById', {'cids' : '1,2'});
+vk.on('done:places.getCountryById', function(_o) {
+    console.log(_o);
 });
 ```
 
 * Первый параметр &mdash; название метода API.
 * Второй &mdash; параметры запроса к методу. Если параметров нет &mdash; передайте пустой объект.
-* Третий параметр &mdash; callback, которому будет передан ответ API.
+
+При завершении обращения к API библиотека сгенерирует событие вида done:methodName. В коде выше запрашиваются
+два метода (getProfiles, places.getCountryById), а по результатам генерируются два события (done:getProfiles,
+done:places.getCountryById). Вместе с событием передается ответ API.
+
+Поддержка
+-------
+* 57uff3r@gmail.com
+* http://vk.com/s7uff3r
--- vksdk.js
+++ vksdk.js
@@ -2,81 +2,82 @@
  * @author 57uff3r@gmail.com
  * @see https://github.com/57uff3r/nodejs-vksdk
  */
+var     util            = require('util');
+        EventEmitter    = require('events').EventEmitter,
+        crypto          = require('crypto'),
+        http            = require('http');
+
+var VK = function(_appID, _appSecret) {
+    var self = this;
+
+    self.appID          = _appID;
+    self.appSecret     = _appSecret;
+
+    self.request = function(method, params) {
+            params.api_id           = self.appID;
+            params.v                = '3.0';
+            params.method           = method;
+            params.timestamp        = new Date().getTime();
+            params.format           = 'json';
+            params.random           = Math.floor(Math.random() * 9999)
+
+            params  = this._sortObjectByKey(params);
+            var sig = '';
+            for(key in params) {
+                    sig = sig + key + '=' + params[key];
+            }
+            sig             = sig + self.appSecret;
+            params.sig      = crypto.createHash('md5').update(sig, 'utf8').digest('hex');
 
 
-module.exports = {
-        'init' : function(appID, appSecret) {
-                this.appID      = appID;
-                this.appSecret  = appSecret;
-                this.crypto     = require('crypto');
-                this.http       = require('http');
-        },
+            var requestArray = new Array();
+            for(key in params) requestArray.push(key + '=' + (params[key]) );
+            var requestString = this._implode('&', requestArray);
 
-        'request' : function(method, params, callback) {
-                this.callback = callback;
-                params.api_id           = this.appID;
-                params.v                = '3.0';
-                params.method           = method;
-                params.timestamp        = new Date().getTime();
-                params.format           = 'json';
-                params.random           = Math.floor(Math.random() * 9999)
+            var options = {
+                    host: 'api.vk.com',
+                    port: 80,
+                    path: '/api.php?' + requestString,
+            };
+            http.get(options, function(res) {
+                    var apiResponse = new String();
+                    res.setEncoding('utf8');
 
-                params  = this._sortObjectByKey(params);
-                var sig = '';
-                for(key in params) {
-                        sig = sig + key + '=' + params[key];
-                }
-                sig             = sig + this.appSecret;
-                params.sig      = this.crypto.createHash('md5').update(sig, 'utf8').digest('hex');
+                    res.on('data', function(chunk) {
+                            apiResponse += chunk;
+                    });
 
+                    res.on('end',  function() {
+                            var o = JSON.parse(apiResponse);
+                            self.emit('done:' + method, o);
+                    });
 
-                var requestArray = new Array();
-                for(key in params) {
-                    if( key == "message" ) requestArray.push(key + '=' + encodeURIComponent(params[key]) );
-					else requestArray.push(key + '=' + (params[key]) );
-                }
-                var requestString = this._implode('&', requestArray);
+            });
+    };
 
-                var options = {
-                        host: 'api.vk.com',
-                        port: 80,
-                        path: '/api.php?' + requestString,
-                };
-                this.http.get(options, function(res) {
-                        var apiResponse = new String();
-                        res.setEncoding('utf8');
+    self._implode  = function implode( glue, pieces ) {
+            return ( ( pieces instanceof Array ) ? pieces.join ( glue ) : pieces );
+    };
 
-                        res.on('data', function(chunk) {
-                                apiResponse += chunk;
-                        });
+    self._sortObjectByKey = function (o) {
+            var sorted = {},
+            key, a = [];
 
-                        res.on('end',  function() {
-                                var o = JSON.parse(apiResponse);
-                                module.exports.callback(o);
-                        });
+            for (key in o) {
+                    if (o.hasOwnProperty(key)) {
+                            a.push(key);
+                    }
+            }
 
-                });
-        },
+            a.sort();
 
-        '_implode' : function implode( glue, pieces ) {
-                return ( ( pieces instanceof Array ) ? pieces.join ( glue ) : pieces );
-        },
+            for (key = 0; key < a.length; key++) {
+                    sorted[a[key]] = o[a[key]];
+            }
+            return sorted;
+    };
 
-        '_sortObjectByKey' : function (o) {
-                var sorted = {},
-                key, a = [];
+}
 
-                for (key in o) {
-                        if (o.hasOwnProperty(key)) {
-                                a.push(key);
-                        }
-                }
-
-                a.sort();
-
-                for (key = 0; key < a.length; key++) {
-                        sorted[a[key]] = o[a[key]];
-                }
-                return sorted;
-        }
-};
+util.inherits(VK, EventEmitter);
+module.exports = VK;
