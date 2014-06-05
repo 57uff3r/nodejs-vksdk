7a4d692aa744f0b012e932bb8de63b13d5504b42
Andrey Korchak <57uff3r@gmail.com>
2014-06-05 23:36:21 +0700

    Update tests

--- test/index.js
+++ test/index.js
@@ -13,16 +13,86 @@ var done = function(_o) {
 };
 
 describe('api', function() {
+    this.timeout(15000);
 
-    it('should load a user profile', function(done) {
-        assert.doesNotThrow(function() {
+    it('Should load a user profile', function(done) {
+      assert.doesNotThrow(function() {
 
-            vk.request('getProfiles', {'uids' : '29894'});
-            vk.on('done:getProfiles', function(_o) {
-                assert.equal(_o.response[0].uid, 29894);
-                done();
-            });
-        });
-
+          vk.request('getProfiles', {'uids' : '29894'});
+          vk.on('done:getProfiles', function(_o) {
+            assert.equal(_o.response[0].uid, 29894);
+            done();
+          });
+      });
     });
+
+
+    it('Should load a country info', function(done) {
+      assert.doesNotThrow(function() {
+
+        vk.request('places.getCountryById', {'cids' : '1,2'});
+        vk.on('done:places.getCountryById', function(_o) {
+          assert.equal(_o.response[0].cid, 1);
+          assert.equal(_o.response[1].cid, 2);
+          done();
+        });
+      });
+    });
+
+
+    it('Should return an error', function(done) {
+      assert.doesNotThrow(function() {
+
+        vk.request('secure.getAppBalance', {'cids' : '1,2'});
+        vk.on('done:secure.getAppBalance', function(_o) {
+          assert.equal(_o.error.error_code, 500);
+          done();
+        });
+      });
+    });
+
+
+    it('Should fire a custom event', function(done) {
+      assert.doesNotThrow(function() {
+
+        vk.request('places.getCountryById', {'cids' : '1,2'}, 'myCustomEventName');
+        vk.on('myCustomEventName', function(_o) {
+          assert.equal(_o.response[0].cid, 1);
+          assert.equal(_o.response[1].cid, 2);
+          done();
+        });
+      });
+    });
+
+
+    it('Should return an error', function(done) {
+      assert.doesNotThrow(function() {
+
+        vk.acquireToken('57uff3r@gmail.com', 'test');
+        vk.on('acquireTokenNotReady', function(_o) {
+          assert.equal(_o.error, 'invalid_request');
+          done();
+        });
+      });
+    });
+
+
+    it('Server side request: should return error', function(done) {
+      assert.doesNotThrow(function() {
+        vk.changeMode('oauth');
+        vk.setToken();
+        vk.on('appServerTokenReady', function(_o) {
+          assert.equal(true, !!vk.getToken());
+          done();
+
+          vk.request('secure.getAppBalance');
+          vk.on('done:secure.getAppBalance', function(_o) {
+            assert.equal(_o.error.error_code, 500);
+          });
+        });
+      });
+    });
+
 });
+
+
